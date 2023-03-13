c0ac09488ca4c47d8ea3530b236e28110c10b8bf
Julia <102997951+idhnyy@users.noreply.github.com>
2023-03-13 20:17:47 -0300

    Add files via upload

---
+++ StoryTellingApp88/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCx0RuAknyOeelB3dQ8o6PfKPIwmZO0aM",
+  authDomain: "storytelling-d85f2.firebaseapp.com",
+  databaseURL: "https://storytelling-d85f2-default-rtdb.firebaseio.com",
+  projectId: "storytelling-d85f2",
+  storageBucket: "storytelling-d85f2.appspot.com",
+  messagingSenderId: "703376405356",
+  appId: "1:703376405356:web:66fb3e1763ce7e4c043949"
+};
